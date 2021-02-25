134b4ae6261795da358f967befc4e59d0b7c7934
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-02-25 21:18:48 +0100

    Refactoring

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import { reverseMap } from './reverseMap.js'
 import { createMusicalNotation } from './musical_notation.js'
+import { reverseMap } from './reverseMap.js'
 
 const keys = new Map([
+  // [<note name>, <MIDI note number>]
   ['C', 48],
   ['D', 50],
   ['E', 52],
@@ -11,11 +12,12 @@ const keys = new Map([
   ['B', 59],
 ])
 
-const pitchToNote = reverseMap(keys)
+const midiNumberToNoteName = reverseMap(keys)
 
 // tempo: … BPM
 // time signature: 4/4
 const melody = [
+  // [<note name>, <note length>]
   ['C', 1/4],
   ['D', 1/4],
   ['E', 1/4],
@@ -45,49 +47,144 @@ const melody = [
   ['C', 1],
 ]
 
-async function main() {
-  const container = document.createElement('div')
-  container.classList.add('note')
-  document.body.appendChild(container)
+const MidiMessageEvent = {
+  getNoteName(midiMessageEvent) {
+    const midiData = midiMessageEvent.data
+    const number = midiData[1]
+    return midiNumberToNoteName.get(number)
+  }
+}
 
-  const musicalNotationContainer = document.createElement('div')
-  document.body.appendChild(musicalNotationContainer)
+class Piano {
+  constructor(noteContainer, musicalNotationContainer) {
+    this.noteContainer = noteContainer
+    this.musicalNotationContainer = musicalNotationContainer
+    this.index = 0
 
-  let index = 0
+    this.onMidiMessage = this.onMidiMessage.bind(this)
+  }
 
-  async function renderNote() {
-    if (index < melody.length) {
-      const note = melody[index]
-      container.textContent = note[0]
+  async run() {
+    await this.renderCurrentNote()
 
-      const musicalNotation = await createMusicalNotation([note])
-      musicalNotationContainer.innerHTML = ''
-      musicalNotationContainer.appendChild(musicalNotation)
+    const midiAccess = await navigator.requestMIDIAccess()
+    const input = Array.from(midiAccess.inputs.values())[1]
+    input.addEventListener('midimessage', this.onMidiMessage)
+  }
+
+  async renderCurrentNote() {
+    if (this.isMelodyInProgress()) {
+      await this.renderNote(this.getCurrentNote())
     } else {
-      container.textContent = ''
-      musicalNotationContainer.innerHTML = ''
+      this.clearOutput()
     }
   }
 
-  await renderNote()
+  isMelodyInProgress() {
+    return this.index < melody.length
+  }
 
-  const midiAccess = await navigator.requestMIDIAccess()
-  const input = Array.from(midiAccess.inputs.values())[1]
-  input.addEventListener('midimessage', async function (event) {
-    const data = event.data
-    const code = data[0]
-    const NOTE_ON = 144
-    if (code === NOTE_ON) {
-      const pitch = data[1]
-      if (pitchToNote.has(pitch)) {
-        const note = pitchToNote.get(pitch)
-        if (note === melody[index][0]) {
-          index++
-          await renderNote()
-        }
-      }
+  getCurrentNote() {
+    return melody[this.index]
+  }
+
+  async renderNote(note) {
+    const noteName = note[0]
+    this.updateNote(noteName)
+    await this.updateMusicalNotation(note)
+  }
+
+  updateNote(noteName) {
+    this.noteContainer.textContent = noteName
+  }
+
+  async updateMusicalNotation(note) {
+    const musicalNotation = await createMusicalNotation([note])
+    this.musicalNotationContainer.innerHTML = ''
+    this.musicalNotationContainer.appendChild(musicalNotation)
+  }
+
+  clearOutput() {
+    this.clearNote()
+    this.clearMusicalNotation()
+  }
+
+  clearNote() {
+    this.noteContainer.textContent = ''
+  }
+
+  clearMusicalNotation() {
+    this.musicalNotationContainer.innerHTML = ''
+  }
+
+  async onMidiMessage (midiMessageEvent) {
+    if (this.isNoteOnMidiMessageEvent(midiMessageEvent)) {
+      await this.onNoteOn(midiMessageEvent)
     }
-  })
+  }
+
+  isNoteOnMidiMessageEvent(midiMessageEvent) {
+    const midiData = midiMessageEvent.data
+    const command = midiData[0]
+    return this.isNoteOnCommand(command)
+  }
+
+  async onNoteOn(midiMessageEvent) {
+    if (this.isCurrentMelodyNotePlayed(midiMessageEvent)) {
+      this.nextNote()
+    }
+  }
+
+  isCurrentMelodyNotePlayed(midiMessageEvent) {
+    const playedNoteName = MidiMessageEvent.getNoteName(midiMessageEvent)
+    const currentNoteName = this.getCurrentNoteName()
+    return playedNoteName === currentNoteName
+  }
+
+  isNoteOnCommand(command, NOTE_ON) {
+    const NOTE_ON = 144
+    return command === NOTE_ON
+  }
+
+  getCurrentNoteName() {
+    return this.getNoteName(this.getCurrentNote())
+  }
+
+  getNoteName(note) {
+    return note[0]
+  }
+
+  nextNote() {
+    this.index++
+    await this.renderCurrentNote()
+  }
+}
+
+async function main() {
+  const root = document.body
+
+  const noteContainer = createNoteContainer()
+  root.appendChild(noteContainer)
+
+  const musicalNotationContainer = createMusicalNotationContainer()
+  root.appendChild(musicalNotationContainer)
+
+  const piano = new Piano(
+    noteContainer,
+    musicalNotationContainer
+  )
+
+  await piano.run()
+}
+
+function createNoteContainer() {
+  const container = document.createElement('div')
+  container.classList.add('note')
+  return container
+}
+
+function createMusicalNotationContainer() {
+  return document.createElement('div')
 }
 
 main()
